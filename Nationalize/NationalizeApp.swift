7c52e76db303a385adfadf9f33b4f366647157b0
kramarivan <99kramar@gmail.com>
2024-10-31 22:29:19 +0100

    Initial Commit

---
+++ Nationalize/NationalizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  NationalizeApp.swift
+//  Nationalize
+//
+//  Created by Ivan Kramar on 31.10.2024..
+//
+
+import SwiftUI
+
+@main
+struct NationalizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NationalizeTests/NationalizeTests.swift
@@ -0,0 +1,17 @@
+//
+//  NationalizeTests.swift
+//  NationalizeTests
+//
+//  Created by Ivan Kramar on 31.10.2024..
+//
+
+import Testing
+@testable import Nationalize
+
+struct NationalizeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
